eebfd6e87ed01178f01c35dd7c92fddaff5ead9f
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-04-15 15:34:38 +0200

    Allow selection of a full color palette of N colors

--- tudplot/__init__.py
+++ tudplot/__init__.py
@@ -1,5 +1,8 @@
-import matplotlib
+import numpy
+import matplotlib as mpl
 from matplotlib import pyplot
+#from matplotlib.colors import LinearSegmentedColormap
+
 from .xmgrace import export_to_agr, load_agr_data
 
 # Official TUD Colors
@@ -31,12 +34,19 @@ def activate(scheme='b', full=False, **kwargs):
         scheme (opt.): Color scheme to activate, default is 'b'.
         full (opt.):
             Activate the full color palette. If False a smaller color palette is used.
+            If a number N is given, N colors will be chosen based on a interpolation of
+            all tudcolors.
         **kwargs: Any matplotlib rc paramter may be given as keyword argument.
     """
     import seaborn as sns
     sns.reset_defaults()
     if full:
-        colors = tudcolors[scheme]
+        if isinstance(full, int):
+            cmap = mpl.colors.LinearSegmentedColormap.from_list('tud{}'.format(scheme),
+                                                                tudcolors[scheme])
+            colors = cmap(numpy.linspace(0, 1, full))
+        else:
+            colors = tudcolors[scheme]
     else:
         colors = [tudcolors[scheme][i] for i in [1, 8, 3, 9, 6, 2]]
     sns.set_palette(sns.color_palette(colors, len(colors)), len(colors))
@@ -57,12 +67,10 @@ def saveagr(filename, figure=None):
 
 
 def markfigure(x, y, s, **kwargs):
-    from matplotlib import rcParams
-    
     kwargs['transform'] = pyplot.gca().transAxes
     kwargs['ha'] = 'center'
     kwargs['va'] = 'center'
-    kwargs['fontsize'] = rcParams['font.size']*1.25
+    kwargs.setdefault('fontsize', 'large')
 
     pyplot.text(x, y, s, **kwargs)
 

1c2d4f0f254fd79db4ceb80d0f2f43fbe1026c9d
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-09-19 09:57:26 +0200

    Introduced new attribute classes, replacing the weird dict syntax. Also processing matplotlib text artists and converting coordinates.

--- tudplot/xmgrace.py
+++ tudplot/xmgrace.py
@@ -46,10 +46,34 @@ def indexed(list, default=None):
     return index
 
 
-def get_world(line):
-    xmin, xmax = line.get_xlim()
-    ymin, ymax = line.get_ylim()
+def get_viewport_coords(artist):
+    """
+    Get the viewport coordinates of an artist.
+    """
+    trans = artist.axes.transAxes.inverted()
+    return trans.transform(artist.get_window_extent())
+
+
+def get_world_coords(artist):
+    """
+    Get the world coordinates of an artist.
+    """
+    trans = artist.axes.transData.inverted()
+    return trans.transform(artist.get_window_extent())
+
+
+def get_world(axis):
+    xmin, xmax = axis.get_xlim()
+    ymin, ymax = axis.get_ylim()
     return '{}, {}, {}, {}'.format(xmin, ymin, xmax, ymax)
+
+
+def get_view(axis):
+    box = axis.get_position()
+    fx, fy = axis.figure.get_size_inches()
+    sx = fx / min(fx, fy)
+    sy = fy / min(fx, fy)
+    return '{:.3}, {:.3}, {:.3}, {:.3}'.format(box.xmin*sx, box.ymin*sy, box.xmax*sx, box.ymax*sy)
 
 
 def get_major_ticks(dim):
@@ -73,44 +97,142 @@ agr_attr_lists = {
 }
 
 
-# dict of line attributes:
-# value describes the type of the attribute:
-#     static : a static entry
-#     value : use value
-#     index : use index of value in list
-#     map:    add value to list if necessary
-agr_line_attrs = {
-    'hidden': {'type': 'static', 'fmt': 'hidden false'},
-    'type': {'type': 'static', 'fmt': 'type xy'},
-    'label': {'type': 'value', 'fmt': 'legend "{value}"',
-              'condition': lambda lbl: re.search('_line\d+', lbl) is None},
-    'linestyle': {'type': 'index', 'fmt': 'line {attr} {value}'},
-    'linewidth': {'type': 'value', 'fmt': 'line {attr} {value}'},
-    'color': {'type': 'index map', 'fmt': 'line {attr} {value}'},
-    'marker': {'type': 'index', 'fmt': 'symbol {value}'},
-    'markerfacecolor': {'type': 'index map', 'maplist': 'color', 'fmt': 'symbol color {value}'}
-}
-agr_axis_attrs = {
-    'world': {'type': 'function', 'function': get_world, 'fmt': 'world {value}'},
-    'title': {'type': 'value', 'fmt': 'title "{value}"'},
-    'xlabel': {'type': 'value', 'fmt': 'xaxis label "{value}"'},
-    'ylabel': {'type': 'value', 'fmt': 'yaxis label "{value}"'},
-    'xscale': {'type': 'value', 'fmt': 'xaxes scale Logarithmic',
-               'condition': lambda scale: scale is 'log'},
-    'yscale': {'type': 'value', 'fmt': 'yaxes scale Logarithmic',
-               'condition': lambda scale: scale is 'log'},
-    'xticks': {'type': 'function', 'fmt': 'xaxis tick major {value}',
-               'function': get_major_ticks('x')},
-    'yticks': {'type': 'function', 'fmt': 'yaxis tick major {value}',
-               'function': get_major_ticks('y')},
+def get_legend(axis):
+    if axis.get_legend() is not None:
+        return 'on'
+    else:
+        return 'off'
 
-}
 
-xmgrace_mapping = {
-    'line': {
-        'get_linewidth': {'fmt': '    {} line linewidth {}'}
+def get_legend_position(axis):
+    return '{}, {}'.format(*get_world_coords(axis.get_legend())[[(0, 1), (0, 1)]])
+
+
+def get_text_position(text):
+    return '{}, {}'.format(*get_world_coords(text)[0])
+
+
+class StaticAttribute:
+    """
+    A static attribute that just writes a line to the agr file if it is set.
+    Functions also as a base class for other attribute classes.
+    """
+
+    def __init__(self, key, fmt):
+        """
+        Args:
+            key: The name of the attribute.
+            fmt: The string which is written to the agr file.
+        """
+        self.key = key
+        self.fmt = fmt
+
+    def format(self, source=None):
+        """
+        Return the (formatted) string of the attribute.
+
+        Args:
+            source: The python object, which is only included here for signature reasons
+        """
+        return self.fmt
+
+
+class ValueAttribute(StaticAttribute):
+    """
+    An attribute which writes a key value pair into the agr file.
+    The agr string has the format: '{fmt} {value}'
+    """
+    attr_lists = {
+        'linestyle': ('None', '-', ':', '--', None, '-.', None, None, None),
+        'marker': ('None', 'o', 's', 'd', '^', '<', 'v', '>', '+', 'x', '*'),
+        'color': ['white', 'black'],
     }
-}
+
+    def _get_value(self, source):
+        value = getattr(source, 'get_{}'.format(self.key))()
+        if is_string_like(value):
+            value = latex_to_xmgrace(value)
+            if not self.index:
+                value = '"{}"'.format(value)
+
+        return value
+
+    def __init__(self, *args, index=None, function=None, condition=None):
+        """
+        Args:
+            *args: Arguments of super().__init__()
+            index:
+                True if value should be mapped to an index. If this is a str this will
+                be used as the index lists key.
+            function: A function that is used to fetch the value from the source.
+            condition: A function that decides if the attribute is written to the agr file.
+        """
+        super().__init__(*args)
+
+        if index:
+            if index is True:
+                self.index = self.key
+            else:
+                self.index = index
+            self.attr_lists.setdefault(self.index, [])
+        else:
+            self.index = False
+
+        if function is not None:
+            self._get_value = function
+        self.condition = condition or (lambda x: True)
+
+    def format(self, source):
+        value = self._get_value(source)
+        if not self.condition(value):
+            return None
+        if self.index:
+            attr_list = self.attr_lists[self.index]
+            index = indexed(attr_list)(value)
+            if index is None:
+                try:
+                    attr_list.append(value)
+                    index = attr_list.index(value)
+                except AttributeError:
+                    print('index not found:', value, index, attr_list)
+                    index = 1
+            value = index
+        return ' '.join([self.fmt, str(value)])
+
+agr_line_attrs = [
+    StaticAttribute('hidden', 'hidden false'),
+    StaticAttribute('type', 'type xy'),
+    ValueAttribute('label', 'legend', condition=lambda lbl: re.search(r'\\sl\\Nine\d+', lbl) is None),
+    ValueAttribute('linestyle', 'line linestyle', index=True),
+    ValueAttribute('linewidth', 'line linewidth'),
+    ValueAttribute('color', 'line color', index=True),
+    ValueAttribute('marker', 'symbol', index=True),
+    ValueAttribute('markeredgecolor', 'symbol color', index='color'),
+    ValueAttribute('markerfacecolor', 'symbol fill color', index='color'),
+    ValueAttribute('markeredgewidth', 'symbol linewidth'),
+]
+
+agr_axis_attrs = [
+    ValueAttribute('world', 'world', function=get_world),
+    ValueAttribute('view', 'view', function=get_view),
+    ValueAttribute('title', 'title'),
+    ValueAttribute('xlabel', 'xaxis label'),
+    ValueAttribute('ylabel', 'yaxis label'),
+    ValueAttribute('xscale', 'xaxes scale Logarithmic', condition=lambda scale: 'log' in scale),
+    ValueAttribute('yscale', 'yaxes scale Logarithmic', condition=lambda scale: 'log' in scale),
+    ValueAttribute('xticks', 'xaxis tick major', function=get_major_ticks('x')),
+    ValueAttribute('yticks', 'yaxis tick major', function=get_major_ticks('y')),
+    ValueAttribute('legend', 'legend', function=get_legend),
+    StaticAttribute('legend', 'legend loctype world'),
+    ValueAttribute('legend', 'legend', function=get_legend_position)
+]
+
+agr_text_attrs = [
+    StaticAttribute('string', 'on'),
+    StaticAttribute('string', 'loctype world'),
+    ValueAttribute('position', '', function=get_text_position),
+    ValueAttribute('text', 'def')
+]
 
 
 class AgrFile:
@@ -137,36 +259,8 @@ class AgrFile:
             file.write(self.body)
             file.write(self.tail)
 
-patterns = {
-    '\$': '',
-    r'\^({.+}|.)': r'\S\1\N',
-    r'\_({.+}|.)': r'\s\1\N',
-}
 
-# Greek letters in xmgrace are written by switching to symbol-font:
-# "\x a\f{}" will print an alpha an switch back to normal font
-greek = {
-    'alpha': 'a', 'beta': 'b', 'gamma': 'g', 'delta': 'd', 'epsilon': 'e', 'zeta': 'z',
-    'eta': 'h', 'theta': 'q', 'iota': 'i', 'kappa': 'k',  'lambda': 'l',  'mu': 'm',
-    'nu': 'n', 'xi': 'x', 'omicron': 'o', 'pi': 'p', 'rho': 'r', 'sigma': 's',
-    'tau': 't', 'upsilon': 'u', 'phi': 'f', 'chi': 'c', 'psi': 'y', 'omega': 'w',
-    'varphi': 'j', 'varepsilon': 'e', 'vartheta': 'J', 'varrho': 'r'
-}
-for latex, xmg in greek.items():
-    patt = r'\\{}'.format(latex)
-    repl = r'\\x {}\\f{{}}'.format(xmg)
-    patterns[patt] = repl
-
-
-def latex_to_xmgrace(string):
-
-    for patt, repl in patterns.items():
-        string = re.sub(patt, repl, string)
-
-    return string
-
-
-def process_attributes(attrs, source, agr, prefix=''):
+def _process_attributes(attrs, source, agr, prefix=''):
     for attr, attr_dict in attrs.items():
         attr_type = attr_dict['type']
         if 'static' in attr_type:
@@ -194,19 +288,27 @@ def process_attributes(attrs, source, agr, prefix=''):
         agr.writeline(prefix + attr_dict['fmt'], attr=attr, value=value)
 
 
+def process_attributes(attrs, source, agr, prefix=''):
+    for attr in attrs:
+        fmt = attr.format(source)
+        if fmt is not None:
+            agr.writeline(prefix + fmt)
+
+
 def export_to_agr(figure, filename):
     """
     Export a matplotlib figure to xmgrace format.
     """
     cc = ColorConverter()
     agr = AgrFile()
-    agr_attr_lists['color'] = ['white', 'black']
+    # agr_attr_lists['color'] = ['white', 'black']
     # agr_colors =
-
+    papersize = figure.get_size_inches()*150
+    agr.writeline('page size {}, {}'.format(*papersize))
     for i, axis in enumerate(figure.axes):
 
         agr_axis = 'g{}'.format(i)
-
+        agr.indent = 0
         agr.writeline('{axis} on', axis=agr_axis)
         agr.writeline('{axis} hidden false')
         agr.writeline('{axis} type XY')
@@ -221,12 +323,27 @@ def export_to_agr(figure, filename):
             process_attributes(agr_line_attrs, line, agr, '{line} ')
             agr.writedata(line.get_xydata())
 
+        for text in axis.texts:
+            agr.indent = 0
+            agr.writeline('with string')
+            agr.indent = 4
+            process_attributes(agr_text_attrs, text, agr, 'string ')
+
     agr.indent = 0
-    for i, color in enumerate(agr_attr_lists['color']):
+    tudcol_rev = {}
+    for style, colors in tudcolors.items():
+        for i, col in enumerate(colors):
+            rgba, = cc.to_rgba_array(col)
+            tudcol_rev[tuple(rgba)] = 'tud{}{}'.format(i, style)
+
+    for i, color in enumerate(ValueAttribute.attr_lists['color']):
+        # print(i, color)
         if color is not 'none':
-            rgb_tuple = tuple(int(255 * c) for c in cc.to_rgba_array(color)[0, :3])
+            rgba, = cc.to_rgba_array(color)
+            rgb_tuple = tuple(int(255 * c) for c in rgba[:3])
+            color_name = tudcol_rev.get(tuple(rgba), color)
             agr.writeline('map color {index} to {rgb}, "{color}"',
-                          part='head', index=i, rgb=rgb_tuple, color=color)
+                          part='head', index=i, rgb=rgb_tuple, color=color_name)
 
     agr.save(filename)
 

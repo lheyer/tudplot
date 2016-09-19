becb14cc84f1fa5a78f7f400776f76170b66e290
Niels Müller <niels.mueller@physik.tu-darmstadt.de>
2016-09-19 09:52:43 +0200

    Moved TUD-Style stuff into a separate module.

---
+++ tudplot/tud.py
@@ -0,0 +1,19 @@
+
+tudcolors = {
+    'a': ('#5D85C3', '#009CDA', '#50B695', '#AFCC50', '#DDDF48', '#FFE05C',
+          '#F8BA3C', '#EE7A34', '#E9503E', '#C9308E', '#804597'),
+    'b': ('#005AA9', '#0083CC', '#009D81', '#99C000', '#C9D400', '#FDCA00',
+          '#F5A300', '#EC6500', '#E6001A', '#A60084', '#721085'),
+    'c': ('#004E8A', '#00689D', '#008877', '#7FAB16', '#B1BD00', '#D7AC00',
+          '#D28700', '#CC4C03', '#B90F22', '#951169', '#611C73'),
+    'd': ('#243572', '#004E73', '#00715E', '#6A8B22', '#99A604', '#AE8E00',
+          '#BE6F00', '#A94913', '#961C26', '#732054', '#4C226A'),
+}
+tudstyle = {
+    'font.family': 'Charter',
+    'lines.linewidth': 1.5,
+    'lines.markeredgewidth': 1.5,
+    'lines.markersize': 6,
+    'figure.figsize': (6, 4),
+    'markers.fillstyle': 'none',
+}
